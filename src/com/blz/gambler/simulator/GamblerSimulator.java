1d9dee7967aee1f76b97db4eb5228c29d2202e7e
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-28 15:29:38 +0530

    Added: To find total amount for 20 days

--- src/com/blz/gambler/simulator/GamblerSimulator.java
+++ src/com/blz/gambler/simulator/GamblerSimulator.java
@@ -6,7 +6,7 @@ public class GamblerSimulator {
 	public static final int STAKE = 100;
 	public static final int BET = 1;
 	public static final float STAKE_VALUE = 0.5f;
-	int stake = 100, lossAmount, winAmount;
+	int stake = 100, lossAmount, winAmount, totalAmountEarned = 0;
 
 	public int winOrLoss() {
 		Random rand = new Random();
@@ -20,28 +20,38 @@ public class GamblerSimulator {
 		}
 		return stake;
 	}
-	
+
 	public int resignStake() {
-		lossAmount = (int)Math.round(STAKE * STAKE_VALUE);
-		winAmount = (int)Math.round(STAKE + (STAKE * STAKE_VALUE));
+		lossAmount = (int) Math.round(STAKE * STAKE_VALUE);
+		winAmount = (int) Math.round(STAKE + (STAKE * STAKE_VALUE));
 		boolean stop = true;
 		stake = STAKE;
-		while(stop == true) {
+		while (stop == true) {
 			winOrLoss();
-			if(stake == lossAmount) {
+			if (stake == lossAmount) {
 				stop = false;
 			}
-			if(stake == winAmount){
+			if (stake == winAmount) {
 				stop = false;
 			}
 		}
 		return stake;
 	}
 
+	public int getTotalAmountWonOrLoss() {
+		int day_stake = 0;
+		for (int day = 0; day <= 20; day++) {
+			day_stake = resignStake();
+			totalAmountEarned = totalAmountEarned + day_stake;
+		}
+		System.out.println("Total Amount Earned or Loss by Gambler at End of given period:- " + totalAmountEarned);
+		return totalAmountEarned;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO GAMBLER SIMULATION PROBLEM");
 		GamblerSimulator game = new GamblerSimulator();
-		System.out.println("Remaining Stake: " + game.resignStake());
+		game.getTotalAmountWonOrLoss();
 	}
 
 }
