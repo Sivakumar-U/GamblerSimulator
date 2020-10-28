260846832d1e82b4f7cc1a6bf7df6b1440371be6
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-28 15:00:34 +0530

    Added: Gambler simulation starts with stake and bet

---
+++ src/com/blz/gambler/simulator/GamblerSimulator.java
@@ -0,0 +1,11 @@
+package com.blz.gambler.simulator;
+
+public class GamblerSimulator {
+	public static final int STAKE = 100;
+	public static final int BET = 1;
+
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO GAMBLER SIMULATION PROBLEM");
+	}
+
+}
